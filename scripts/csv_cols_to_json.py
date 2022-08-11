b9f84af37b7b7b15b5e817447aa6a4354799d287
Matt Fisher <matt.fisher@nsidc.org>
2022-08-11 22:11:05 +0000

    Fixup JSON types

--- scripts/csv_cols_to_json.py
+++ scripts/csv_cols_to_json.py
@@ -5,9 +5,20 @@ representing the column for each header.
 """
 import csv, json, sys
 
-csv_as_list_of_dicts = [dict(r) for r in csv.DictReader(sys.stdin)]
-csv_as_dict_of_lists = {
-    k: [dct[k] for dct in csv_as_list_of_dicts] for k in csv_as_list_of_dicts[0]
-}
 
-print(json.dumps(csv_as_dict_of_lists))
+def _normalize_value(dct, k):
+    """The "index" (doy) is integer, and the values are floats."""
+    if k == 'day_of_water_year':
+        return int(dct[k])
+    else:
+        return float(dct[k])
+
+
+if __name__ == '__main__':
+    csv_as_list_of_dicts = [dict(r) for r in csv.DictReader(sys.stdin)]
+    csv_as_dict_of_lists = {
+        k: [_normalize_value(dct, k) for dct in csv_as_list_of_dicts]
+        for k in csv_as_list_of_dicts[0].keys()
+    }
+
+    print(json.dumps(csv_as_dict_of_lists))
