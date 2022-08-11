c0fc1729849acbd5d6e6755e711267a8a0030b8f
Matt Fisher <matt.fisher@nsidc.org>
2022-08-11 23:12:31 +0000

    Convert NaNs to nulls properly

--- scripts/csv_cols_to_json.py
+++ scripts/csv_cols_to_json.py
@@ -3,15 +3,32 @@
 The output will be a JSON object with the CSV headers as keys. Values will be arrays
 representing the column for each header.
 """
-import csv, json, sys
+import csv, json, math, sys
+
+
+def _nan_to_none(val):
+    if math.isnan(val):
+        return None
+    return val
 
 
 def _normalize_value(dct, k):
-    """The "index" (doy) is integer, and the values are floats."""
+    """The "index" (doy) is integer, and the values are floats.
+
+    Also convert NaNs to None.
+    """
+    val = dct[k]
     if k == 'day_of_water_year':
-        return int(dct[k])
+        return int(val)
     else:
-        return float(dct[k])
+        float_val = float(val)
+
+        # The Python JSON encoder outputs NaNs, which are _not_ valid JSON.
+        # Convert to nulls:
+        if math.isnan(float_val):
+            return None
+
+        return float_val
 
 
 if __name__ == '__main__':
@@ -21,8 +38,4 @@ if __name__ == '__main__':
         for k in csv_as_list_of_dicts[0].keys()
     }
 
-    print(json.dumps(
-        csv_as_dict_of_lists,
-        # Convert NaNs to nulls:
-        ignore_nan=True,
-    ))
+    print(json.dumps(csv_as_dict_of_lists))
