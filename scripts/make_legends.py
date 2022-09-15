c081ac37da5815ea0bcb7893278b058c73febbf8
Matt Fisher <matt.fisher@nsidc.org>
2022-09-15 16:29:02 +0000

    Calculate legend extend property dynamically

--- scripts/make_legends.py
+++ scripts/make_legends.py
@@ -1,6 +1,8 @@
 import copy
 import json
 from pathlib import Path
+from pprint import pprint
+from typing import Literal
 
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -13,10 +15,40 @@ LEGEND_DIMENSIONS = (5, 0.5)
 LEGEND_COLORBAR_BOTTOM = 0.5
 LEGEND_FONT_SIZE = 8
 
+ColormapExtendChoice = Literal['both', 'neither', 'min', 'max']
+
+
+def _extend_str(left: bool, right: bool) -> ColormapExtendChoice:
+    if left and right:
+        return 'both'
+    if left:
+        return 'min'
+    if right:
+        return 'max'
+    return 'neither'
+
+
+def _extend(variable: dict) -> ColormapExtendChoice:
+    cmap_range = variable['colormap_value_range'].copy()
+    data_range = variable['value_range'].copy()
+
+    if cmap_range[0] == 1 and variable['transparent_zero']:
+        cmap_range[0] = 0
+
+    return _extend_str(
+        left=data_range[0] < cmap_range[0],
+        right=data_range[1] > cmap_range[1],
+    )
+
 
 def legend_from_variable(variable_id: str, variable: dict) -> Path:
     """Write a legend based on `variable` and return its path."""
-    mpl.rcParams.update({'font.size': LEGEND_FONT_SIZE})
+    mpl.rcParams.update({
+        'font.size': LEGEND_FONT_SIZE,
+        # Make internal SVG unique IDs deterministic for better diffing:
+        # https://matplotlib.org/stable/tutorials/introductory/customizing.html
+        'svg.hashsalt': 'snow-today',
+    })
 
     label = variable['label_map_legend']
     cmap_range = variable['colormap_value_range']
@@ -33,12 +65,15 @@ def legend_from_variable(variable_id: str, variable: dict) -> Path:
     cmap = LinearSegmentedColormap.from_list('custom_colormap', cmap_values)
     norm = mpl.colors.Normalize(vmin=cmap_range[0], vmax=cmap_range[1])
 
+    # NOTE: The top and bottom values may not always be displayed on the colorbar.
+    # Should we address this?
+    # https://stackoverflow.com/questions/54201881/add-top-and-bottom-value-label-to-color-bar
     fig.colorbar(
         mpl.cm.ScalarMappable(norm=norm, cmap=cmap),
         cax=ax,
         orientation='horizontal',
         label=label,
-        extend='both',
+        extend=_extend(variable),
     )
 
     output_fn = f'{variable_id}.svg'
@@ -47,13 +82,16 @@ def legend_from_variable(variable_id: str, variable: dict) -> Path:
     plt.savefig(
         output_path,
 
-        # Reduce the whitespace on the edges
+        # Reduce the whitespace on the edges:
         bbox_inches='tight',
         pad_inches=0.05,
 
         # Prevent SVG raster element (color gradient) from being misaligned from vector
         # elements:
         dpi=200,
+
+        # Make the outputs more diffable:
+        metadata={'Date': None},
     )
     return output_path
 
@@ -85,7 +123,12 @@ if __name__ == '__main__':
 
     diff = DeepDiff(variables_index, updated_index)
     if diff != {}:
+        print('WARNING: Difference detected in `variables.json`:')
+        pprint(diff)
+        print()
+        input(
+            'Press Enter to overwrite `variables.json`. NOTE: indentation will be'
+            ' borked by this process. CTRL+C to exit without overwriting.'
+        )
         with open(VARIABLES_INDEX_FP, 'w') as f:
             json.dump(updated_index, f, indent=2)
-
-        print('WARNING: `variables.json` overwritten. The indentation will be borked.')
