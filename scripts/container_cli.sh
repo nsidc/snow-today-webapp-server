496073d40ff247f38ba161a44abc46feb7b91f64
Matt Fisher <matt.fisher@nsidc.org>
2022-09-16 19:56:15 +0000

    Update TODOs for container_cli script

--- scripts/container_cli.sh
+++ scripts/container_cli.sh
@@ -4,11 +4,11 @@ THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 REPO_DIR="$(realpath "$THIS_DIR/..")"
 
 cd $REPO_DIR
+# TODO: Conditionally source VERSION.latest.env if not in prod? Look at $ENVIRONMENT?
 source VERSION.env
 DOCKER_IMAGE="nsidc/snow-today-webapp-server-ingest:${SERVER_VERSION}"
 if [[ "$(docker images -q "${DOCKER_IMAGE}" 2>/dev/null)" == "" ]]; then
     echo "Docker image ${DOCKER_IMAGE} not found. Did you deploy it?"
 fi
-# TODO: Specify --no-pull / --no-build so we only run the image
 echo "$@"
 # docker-compose run ingest "$@"
