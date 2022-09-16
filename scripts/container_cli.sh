522268816661d3efbba9adf55294eb298b70af06
Matt Fisher <matt.fisher@nsidc.org>
2022-09-16 00:36:06 +0000

    Add TODOs

--- scripts/container_cli.sh
+++ scripts/container_cli.sh
@@ -4,4 +4,6 @@ THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 REPO_DIR="$(realpath "$THIS_DIR/..")"
 
 cd $REPO_DIR
+# TODO: Source VERSION file
+# TODO: Specify --no-pull / --no-build so we only run the image
 docker-compose run ingest "$@"
