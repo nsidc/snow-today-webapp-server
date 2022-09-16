c455fac4a3fa4d8c70ea1f046571e3352cb794ac
Matt Fisher <matt.fisher@nsidc.org>
2022-09-16 21:24:56 +0000

    Add environment-specific behaviors to container cli script

--- scripts/container_cli.sh
+++ scripts/container_cli.sh
@@ -4,8 +4,15 @@ THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 REPO_DIR="$(realpath "$THIS_DIR/..")"
 
 cd $REPO_DIR
-# TODO: Conditionally source VERSION.latest.env if not in prod? Look at $ENVIRONMENT?
-source VERSION.env
+
+if [ "${ENVIRONMENT}" = "dev" ]; then
+    source VERSION.dev.env
+elif [ "${ENVIRONMENT}" = "integration" ]; then
+    source VERSION.latest.env
+else
+    source VERSION.env
+fi
+
 DOCKER_IMAGE="nsidc/snow-today-webapp-server-ingest:${SERVER_VERSION}"
 if [[ "$(docker images -q "${DOCKER_IMAGE}" 2>/dev/null)" == "" ]]; then
     echo "Docker image ${DOCKER_IMAGE} not found. Did you deploy it?"
