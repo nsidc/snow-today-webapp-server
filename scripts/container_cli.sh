cc39e9321c153df0186ce88b965d877886b25299
Matt Fisher <matt.fisher@nsidc.org>
2022-09-16 19:54:42 +0000

    Populate container_cli test logic

--- scripts/container_cli.sh
+++ scripts/container_cli.sh
@@ -4,6 +4,11 @@ THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 REPO_DIR="$(realpath "$THIS_DIR/..")"
 
 cd $REPO_DIR
-# TODO: Source VERSION file
+source VERSION.env
+DOCKER_IMAGE="nsidc/snow-today-webapp-server-ingest:${SERVER_VERSION}"
+if [[ "$(docker images -q "${DOCKER_IMAGE}" 2>/dev/null)" == "" ]]; then
+    echo "Docker image ${DOCKER_IMAGE} not found. Did you deploy it?"
+fi
 # TODO: Specify --no-pull / --no-build so we only run the image
-docker-compose run ingest "$@"
+echo "$@"
+# docker-compose run ingest "$@"
