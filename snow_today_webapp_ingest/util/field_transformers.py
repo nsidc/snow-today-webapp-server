83f7c224a6e63654465d8d3169f835685d284e33
Matt Fisher <matt.fisher@nsidc.org>
2024-01-29 12:12:52 -0700

    Remove a tiny bit of dead code

--- snow_today_webapp_ingest/util/field_transformers.py
+++ snow_today_webapp_ingest/util/field_transformers.py
@@ -14,33 +14,3 @@ def float_nan_normalized(inp: str) -> float | None:
     if math.isnan(flt):
         return None
     return flt
-
-
-def state_normalized(state: str) -> str:
-    norm = state.removeprefix('US')
-    return norm
-
-
-def huc2_normalized(huc2: str) -> int | None:
-    """Strip HUC prefix from each value and return integer remainder.
-
-    Return None if value is "N/A"
-    """
-    if huc2 == 'N/A':
-        return None
-
-    norm = huc2.removeprefix('HUC')
-    return int(norm)
-
-
-def huc4_normalized(huc4: str) -> int | None:
-    """Integerize value.
-
-    Unlike HUC2 values, no prefix is attached.
-
-    Return None if value is "N/A".
-    """
-    if huc4 == 'N/A':
-        return None
-
-    return int(huc4)
